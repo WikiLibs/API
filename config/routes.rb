7cc946628167693e4a291ac2676400b51eb83651
Tessari <william.tessari@epitech.eu>
2018-12-12 16:44:08 +0100

    test

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  root 'welcome#index'
   resources :posts
-  root 'home#index'
+  get 'home/index'
   devise_for :users, controllers: {registrations: "registrations"}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
