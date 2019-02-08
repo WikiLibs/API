2117b0432986a0028d95cc0471fa92fa001f0836
Nicolas FRAYSSE <nicolas1.fraysse@epitech.eu>
2019-02-08 15:10:44 +0100

    Initial commit + Start of module API + ModuleManager

---
+++ WikiLibs/Services/ModuleManager.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace WikiLibs.Services
+{
+    public class ModuleManager : API.IModuleManager
+    {
+        private Dictionary<Type, API.IModule> _moduleMap;
+
+        public ModuleManager()
+        {
+        }
+
+        public ModuleManager(ModuleManager other)
+        {
+            _moduleMap = other._moduleMap;
+        }
+
+        public T GetModule<T>() where T : API.IModule
+        {
+            if (!_moduleMap.ContainsKey(typeof(T)))
+                return (default(T));
+            return ((T)_moduleMap[typeof(T)]);
+        }
+
+        public void LoadModule(IMvcBuilder builder, string path)
+        {
+            FileInfo inf = new FileInfo("./Modules/" + path + ".dll");
+
+            if (!inf.Exists)
+                return;
+            Assembly asm = Assembly.LoadFile(inf.FullName);
+            foreach (Type t in asm.GetExportedTypes())
+            {
+                if (t.GetCustomAttribute<API.Module>() != null)
+                {
+                    _moduleMap[t.GetCustomAttribute<API.Module>().RefType] = (API.IModule)Activator.CreateInstance(t);
+                    builder.AddApplicationPart(asm);
+                }
+            }
+        }
+    }
+}
---
+++ WikiLibs/Startup.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace WikiLibs
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            List<string> modules = new List<string>();
+            Services.ModuleManager mgr = new Services.ModuleManager();
+            Configuration.Bind(modules);
+            IMvcBuilder builder = services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            foreach (string s in modules)
+                mgr.LoadModule(builder, s);
+            services.AddScoped<API.IModuleManager>(_ =>
+            {
+                return (new Services.ModuleManager(mgr));
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
