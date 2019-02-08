9de69a10055d277044cac7b557e6a5e3ff02c235
Nicolas FRAYSSE <nicolas1.fraysse@epitech.eu>
2019-02-08 13:27:10 +0100

    Cleanup branch for rewrite in ASP.Net Core

--- config/routes.rb
+++
@@ -1,7 +0,0 @@
-Rails.application.routes.draw do
-  root 'welcome#index'
-  resources :posts
-  get 'home/index'
-  devise_for :users, controllers: {registrations: "registrations"}
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
