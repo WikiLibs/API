06240914254acd98a10004a00c48b82d1953904d
Yuri Edward <nicolas1.fraysse@epitech.eu>
2019-05-19 18:33:26 +0200

    Added initial version of example manager interfaces

---
+++ WikiLibs.Shared/Modules/IExampleRequestManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WikiLibs.Shared.Modules
+{
+    public interface IExampleRequestManager : ICRUDOperations<Data.Models.Examples.ExampleRequest>
+    {
+        IQueryable<Data.Models.Examples.ExampleRequest> GetAll(int symbol);
+        IQueryable<Data.Models.Examples.ExampleRequest> GetAll();
+        Task ApplyRequest(int id);
+    }
+}
