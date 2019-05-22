c634bb962f96c51d8da861421e65ff2dca19494c
Nicolas FRAYSSE <nicolas1.fraysse@epitech.eu>
2019-05-22 20:22:57 +0200

    Added checks in GroupManager to avoid NULL groups in users

--- WikiLibs.Shared/Modules/Admin/IGroupManager.cs
+++ WikiLibs.Shared/Modules/Admin/IGroupManager.cs
@@ -6,6 +6,8 @@ namespace WikiLibs.Shared.Modules.Admin
 {
     public interface IGroupManager : ICRUDOperations<Data.Models.Group>
     {
+        Data.Models.Group DefaultGroup { get; }
+        Data.Models.Group AdminGroup { get; }
         Data.Models.Group Get(string name);
         IQueryable<Data.Models.Group> GetAll();
     }
