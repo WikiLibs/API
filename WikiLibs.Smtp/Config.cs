e7e8008f5f3dcbeb3c62ae0b0fdbbf7a6ce2f930
Nicolas FRAYSSE <nicolas1.fraysse@epitech.eu>
2019-05-22 09:08:51 +0200

    Rewrote smtp manager to Mailjet

--- WikiLibs.Smtp/Config.cs
+++ WikiLibs.Smtp/Config.cs
@@ -8,10 +8,8 @@ namespace WikiLibs.Smtp
     [Configurator]
     public class Config
     {
-        public string Host { get; set; }
-        public int Port { get; set; }
-        public string Password { get; set; }
-        public string Username { get; set; }
+        public string ApiKey { get; set; }
+        public string ApiSecret { get; set; }
         public string FromName { get; set; }
         public string FromEmail { get; set; }
     }
