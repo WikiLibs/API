41cd7d50b190fd06c0e65d36c86a01a7bf2df3ab
William Tessari <william.tessari@epitech.eu>
2019-05-29 21:25:07 +0200

    firsrt

--- WikiLibs.API/UserController.cs
+++ WikiLibs.API/UserController.cs
@@ -4,7 +4,50 @@ using System.Text;
 
 namespace WikiLibs.API
 {
-    public class UserController
+    [Authorize]
+    [Route("/user")]
+    public class UserController : Controller
     {
+         private readonly IUser _user;
+         private readonly IUserManager _ummgr;
+
+         public UserController(IUser usr) {
+             _user = usr
+         }
+
+        [AllowAnonymous]
+        [ProducesResponseType(200, Type = typeof(Models.Output.User))]
+        [AuthorizeApiKey(Flag = AuthorizeApiKey.Standard)]
+        [HttpGet("{uid}")]
+        public IActionResult GetUser([FromRoute] string uid)
+        {
+             var mdl = await _ummgr.GetAsync(uid);
+             return (Json(Models.Output.User.CreateModel(mdl)));
+        }
+
+        [AllowAnonymous]
+        [ProducesResponseType(200, Type = typeof(Models.Output.User))]
+        [AuthorizeApiKey(Flag = AuthorizeApiKey.Standard)]
+        [HttpGet]
+        public IActionResult GetMe()
+        {
+             var mdl = await _ummgr.GetAsync(_user.UserId);
+             return (Json(Models.Output.User.CreateModel(mdl)));
+        }
+
+        [HttpDelete("{uid}")]
+        public async Task<IActionResult> DeleteUser([FromRoute] string uid)
+        {
+            await _ummgr.DeleteAsync(_ummgr.GetAsync(uid));
+            return (Ok());
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteUser()
+        {
+            await _ummgr.DeleteAsync(_ummgr.GetAsync(_user.UserId));
+            return (Ok());
+        }
+
     }
 }
