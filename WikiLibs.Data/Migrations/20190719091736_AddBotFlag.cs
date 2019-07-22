4e94f5860b71ef3dd79bffd50922f5c66c6d56e7
Nicolas FRAYSSE <nicolas1.fraysse@epitech.eu>
2019-07-22 10:02:49 +0200

    Added ExcludeFromCodeCoverage to the DB migration

--- WikiLibs.Data/Migrations/20190719091736_AddBotFlag.cs
+++ WikiLibs.Data/Migrations/20190719091736_AddBotFlag.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System.Diagnostics.CodeAnalysis;
 
 namespace WikiLibs.Data.Migrations
 {
+    [ExcludeFromCodeCoverage]
     public partial class AddBotFlag : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
