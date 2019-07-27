764b3b2c706f2a18bb41bb9c91519a8921d5a77b
Nicolas FRAYSSE <nicolas1.fraysse@epitech.eu>
2019-07-27 20:22:00 +0200

    Added password utils

---
+++ WikiLibs.Shared/Helpers/PasswordOptions.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WikiLibs.Shared.Helpers
+{
+    public class PasswordOptions
+    {
+        public struct PasswordItem
+        {
+            public char Char { get; set; }
+            public int Frequency { get; set; }
+
+            public PasswordItem(char c, int freq)
+            {
+                Char = c;
+                Frequency = freq;
+            }
+        }
+
+        public PasswordItem[] Alphabet { get; set; }
+        public int NumChars { get; set; }
+
+        public static readonly PasswordOptions Standard = new PasswordOptions()
+        {
+            Alphabet = new PasswordItem[]
+            {
+                new PasswordItem('a', 1),
+                new PasswordItem('b', 1),
+                new PasswordItem('c', 1),
+                new PasswordItem('d', 1),
+                new PasswordItem('e', 1),
+                new PasswordItem('f', 1),
+                new PasswordItem('g', 1),
+                new PasswordItem('h', 1),
+                new PasswordItem('i', 1),
+                new PasswordItem('j', 1),
+                new PasswordItem('k', 1),
+                new PasswordItem('l', 1),
+                new PasswordItem('m', 1),
+                new PasswordItem('n', 1),
+                new PasswordItem('o', 1),
+                new PasswordItem('p', 1),
+                new PasswordItem('q', 1),
+                new PasswordItem('r', 1),
+                new PasswordItem('s', 1),
+                new PasswordItem('t', 1),
+                new PasswordItem('u', 1),
+                new PasswordItem('v', 1),
+                new PasswordItem('w', 1),
+                new PasswordItem('x', 1),
+                new PasswordItem('y', 1),
+                new PasswordItem('z', 1),
+
+                new PasswordItem('A', 2),
+                new PasswordItem('B', 2),
+                new PasswordItem('C', 2),
+                new PasswordItem('D', 2),
+                new PasswordItem('E', 2),
+                new PasswordItem('F', 2),
+                new PasswordItem('G', 2),
+                new PasswordItem('H', 2),
+                new PasswordItem('I', 2),
+                new PasswordItem('J', 2),
+                new PasswordItem('K', 2),
+                new PasswordItem('L', 2),
+                new PasswordItem('M', 2),
+                new PasswordItem('N', 2),
+                new PasswordItem('O', 2),
+                new PasswordItem('P', 2),
+                new PasswordItem('Q', 2),
+                new PasswordItem('R', 2),
+                new PasswordItem('S', 2),
+                new PasswordItem('T', 2),
+                new PasswordItem('U', 2),
+                new PasswordItem('V', 2),
+                new PasswordItem('W', 2),
+                new PasswordItem('X', 2),
+                new PasswordItem('Y', 2),
+                new PasswordItem('Z', 2),
+
+                new PasswordItem('0', 3),
+                new PasswordItem('1', 3),
+                new PasswordItem('2', 3),
+                new PasswordItem('3', 3),
+                new PasswordItem('4', 3),
+                new PasswordItem('5', 3),
+                new PasswordItem('6', 3),
+                new PasswordItem('7', 3),
+                new PasswordItem('8', 3),
+                new PasswordItem('9', 3),
+
+                new PasswordItem('-', 4),
+                new PasswordItem('_', 4),
+                new PasswordItem('@', 4),
+                new PasswordItem('*', 4),
+                new PasswordItem('/', 4),
+                new PasswordItem('\\', 4),
+                new PasswordItem('%', 4),
+                new PasswordItem('&', 4),
+                new PasswordItem('#', 4),
+                new PasswordItem('!', 4),
+                new PasswordItem('?', 4),
+                new PasswordItem(',', 4),
+                new PasswordItem('.', 4),
+                new PasswordItem(';', 4),
+                new PasswordItem(':', 4)
+            },
+            NumChars = 8
+        };
+
+        public static readonly PasswordOptions Reinforced = new PasswordOptions()
+        {
+            Alphabet = new PasswordItem[]
+            {
+                new PasswordItem('a', 1),
+                new PasswordItem('b', 1),
+                new PasswordItem('c', 1),
+                new PasswordItem('d', 1),
+                new PasswordItem('e', 1),
+                new PasswordItem('f', 1),
+                new PasswordItem('g', 1),
+                new PasswordItem('h', 1),
+                new PasswordItem('i', 1),
+                new PasswordItem('j', 1),
+                new PasswordItem('k', 1),
+                new PasswordItem('l', 1),
+                new PasswordItem('m', 1),
+                new PasswordItem('n', 1),
+                new PasswordItem('o', 1),
+                new PasswordItem('p', 1),
+                new PasswordItem('q', 1),
+                new PasswordItem('r', 1),
+                new PasswordItem('s', 1),
+                new PasswordItem('t', 1),
+                new PasswordItem('u', 1),
+                new PasswordItem('v', 1),
+                new PasswordItem('w', 1),
+                new PasswordItem('x', 1),
+                new PasswordItem('y', 1),
+                new PasswordItem('z', 1),
+
+                new PasswordItem('A', 2),
+                new PasswordItem('B', 2),
+                new PasswordItem('C', 2),
+                new PasswordItem('D', 2),
+                new PasswordItem('E', 2),
+                new PasswordItem('F', 2),
+                new PasswordItem('G', 2),
+                new PasswordItem('H', 2),
+                new PasswordItem('I', 2),
+                new PasswordItem('J', 2),
+                new PasswordItem('K', 2),
+                new PasswordItem('L', 2),
+                new PasswordItem('M', 2),
+                new PasswordItem('N', 2),
+                new PasswordItem('O', 2),
+                new PasswordItem('P', 2),
+                new PasswordItem('Q', 2),
+                new PasswordItem('R', 2),
+                new PasswordItem('S', 2),
+                new PasswordItem('T', 2),
+                new PasswordItem('U', 2),
+                new PasswordItem('V', 2),
+                new PasswordItem('W', 2),
+                new PasswordItem('X', 2),
+                new PasswordItem('Y', 2),
+                new PasswordItem('Z', 2),
+
+                new PasswordItem('0', 3),
+                new PasswordItem('1', 3),
+                new PasswordItem('2', 3),
+                new PasswordItem('3', 3),
+                new PasswordItem('4', 3),
+                new PasswordItem('5', 3),
+                new PasswordItem('6', 3),
+                new PasswordItem('7', 3),
+                new PasswordItem('8', 3),
+                new PasswordItem('9', 3),
+
+                new PasswordItem('-', 4),
+                new PasswordItem('_', 4),
+                new PasswordItem('@', 4),
+                new PasswordItem('*', 4),
+                new PasswordItem('/', 4),
+                new PasswordItem('\\', 4),
+                new PasswordItem('%', 4),
+                new PasswordItem('&', 4),
+                new PasswordItem('#', 4),
+                new PasswordItem('!', 4),
+                new PasswordItem('?', 4),
+                new PasswordItem(',', 4),
+                new PasswordItem('.', 4),
+                new PasswordItem(';', 4),
+                new PasswordItem(':', 4),
+                new PasswordItem('²', 4),
+
+                new PasswordItem('ù', 5),
+                new PasswordItem('é', 5),
+                new PasswordItem('è', 5),
+                new PasswordItem('$', 5),
+                new PasswordItem('µ', 5),
+                new PasswordItem('£', 5),
+                new PasswordItem('¤', 5),
+                new PasswordItem('}', 5),
+                new PasswordItem('{', 5),
+                new PasswordItem('[', 5),
+                new PasswordItem(']', 5),
+                new PasswordItem('\'', 5),
+                new PasswordItem('(', 5),
+                new PasswordItem(')', 5),
+                new PasswordItem('à', 5),
+                new PasswordItem('^', 5),
+                new PasswordItem('ç', 5),
+                new PasswordItem('"', 5),
+                new PasswordItem('~', 5),
+                new PasswordItem('§', 5),
+                new PasswordItem('<', 5),
+                new PasswordItem('>', 5),
+            },
+            NumChars = 24
+        };
+    }
+}
---
+++ WikiLibs.Shared/Helpers/PasswordUtils.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WikiLibs.Shared.Helpers
+{
+    public class PasswordUtils
+    {
+        private static char RandomChar(PasswordOptions options, Random rand, int freq)
+        {
+            var res = options.Alphabet.OrderBy(e => e.Frequency).Where(e => e.Frequency == freq);
+            var id = rand.Next(0, res.Count() - 1);
+
+            return (res.ElementAt(id).Char);
+        }
+
+        public static string NewPassword(PasswordOptions options)
+        {
+            var rand = new Random();
+            string res = "";
+            int curFreq = 1;
+
+            while (options.NumChars > 0)
+            {
+                res += RandomChar(options, rand, curFreq);
+                --options.NumChars;
+                ++curFreq;
+                if (!options.Alphabet.Any(e => e.Frequency == curFreq))
+                    curFreq = 1;
+            }
+            return (res);
+        }
+    }
+}
