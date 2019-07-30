6403d8f0b1853747dacdc819bd9c7e4af0c8510b
Nicolas Fraysse <nfraysse@codeinsider.fr>
2019-07-30 14:26:13 +0200

    Hotfix missing authorizations for group and api key controller

--- WikiLibs.API/Admin/APIKeyController.cs
+++ WikiLibs.API/Admin/APIKeyController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
@@ -10,6 +11,7 @@ using WikiLibs.Shared.Service;
 
 namespace WikiLibs.API.Admin
 {
+    [Authorize]
     [Route("/admin/apikey")]
     public class APIKeyController : Controller
     {
