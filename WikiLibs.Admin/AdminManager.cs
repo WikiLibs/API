65df23ebee978c5f64c8e79565dd600788b7aa8e
Yuri Edward <nicolas1.fraysse@epitech.eu>
2020-01-06 16:40:36 +0100

    Added missing base API key intended to initialize API server

--- WikiLibs.Admin/AdminManager.cs
+++ WikiLibs.Admin/AdminManager.cs
@@ -47,6 +47,24 @@ namespace WikiLibs.Admin
             var key = manager._context.ApiKeys.FirstOrDefault(x => x.Description == "[WIKILIBS_SUPER_DEV_API_KEY]");
             manager._logger.LogWarning("Development API Key : " + key.Id);
             manager._logger.LogWarning("This development key is intended for development purposes, it has ALL flags enabled and will never expire");
+#else
+            if (!manager._context.ApiKeys.Any(x => x.Description == "[DEFAULT] BaseKey"))
+            {
+                manager.ApiKeyManager.PostAsync(new Data.Models.ApiKey()
+                {
+                    Flags = AuthorizeApiKey.Authentication | AuthorizeApiKey.Registration,
+                    Description = "[DEFAULT] BaseKey",
+                    ExpirationDate = DateTime.MaxValue,
+                    UseNum = -1
+                }).Wait();
+                manager._logger.LogInformation("A base key has been created, it only supports authentication and registration");
+            }
+            var key = manager._context.ApiKeys.FirstOrDefault(x => x.Description == "[DEFAULT] BaseKey");
+            if (key != null)
+            {
+                manager._logger.LogWarning("Base Key is : " + key.Id);
+                manager._logger.LogWarning("We recommend deleting the base key as soon as possible");
+            }
 #endif
             if (!manager._context.Groups.Any(x => x.Name == "Default"))
             {
