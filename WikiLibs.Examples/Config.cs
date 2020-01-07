0a09686e2d801fc04d83e073b89ac5e831466379
Yuri Edward <nicolas1.fraysse@epitech.eu>
2020-01-07 15:43:43 +0100

    Added configuration option to example requests

---
+++ WikiLibs.Examples/Config.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WikiLibs.Shared.Attributes;
+
+namespace WikiLibs.Examples
+{
+    [Configurator]
+    public class Config
+    {
+        public int MaxExampleRequestsPerPage { get; set; }
+    }
+}
