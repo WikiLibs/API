1947588d9f8ce19fa1d0ea4761a36982f382f7b9
Yuri Edward <nicolas1.fraysse@epitech.eu>
2020-06-22 14:46:58 +0200

    Added lightweight symbol to prepare for tree view

---
+++ WikiLibs.Models/Output/Symbols/LightweightSymbol.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WikiLibs.Data.Models.Symbols;
+
+namespace WikiLibs.Models.Output.Symbols
+{
+    public class LightweightSymbol : GetModel<LightweightSymbol, Data.Models.Symbols.Symbol>
+    {
+        public long Id;
+        public string Type;
+        public string FirstPrototype;
+
+        public override void Map(in Data.Models.Symbols.Symbol model)
+        {
+            Id = model.Id;
+            Type = model.Type.DisplayName;
+            FirstPrototype = model.Prototypes.Count > 0 ? model.Prototypes.First().Data : null;
+        }
+    }
+}
