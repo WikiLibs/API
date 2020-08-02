74c8773bb63a3ab8528c47435512aae927eda11c
Yuri Edward <nicolas1.fraysse@epitech.eu>
2020-08-02 16:29:44 +0200

    Added POST Json model

---
+++ WikiLibs.Models/Input/Examples/ExampleCommentCreate.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WikiLibs.Data.Models.Examples;
+
+namespace WikiLibs.Models.Input.Examples
+{
+    public class ExampleCommentCreate : PostModel<ExampleCommentCreate, ExampleComment>
+    {
+        public string Data { get; set; }
+
+        public override ExampleComment CreateModel()
+        {
+            return (new ExampleComment()
+            {
+                Data = Data,
+                CreationDate = DateTime.UtcNow
+            });
+        }
+    }
+}
