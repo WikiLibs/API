fc8c0377ad5c520bec9d611194996dc1ee1c5e64
Yuri Edward <nicolas1.fraysse@epitech.eu>
2020-08-02 16:30:28 +0200

    Added GET Json model

---
+++ WikiLibs.Models/Output/Examples/ExampleComment.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WikiLibs.Models.Output.Examples
+{
+    public class ExampleComment : GetModel<ExampleComment, Data.Models.Examples.ExampleComment>
+    {
+        public long ExampleId { get; set; }
+        public string UserId { get; set; }
+        public string Data { get; set; }
+        public DateTime CreationDate { get; set; }
+
+        public override void Map(in Data.Models.Examples.ExampleComment model)
+        {
+            ExampleId = model.ExampleId;
+            UserId = model.UserId;
+            Data = model.Data;
+            CreationDate = model.CreationDate;
+        }
+    }
+}
