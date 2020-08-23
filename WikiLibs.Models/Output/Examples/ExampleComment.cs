7f0f3ef82ba4b3f4c6f7800c6ff18e20c0c0a042
Yuri Edward <nicolas1.fraysse@epitech.eu>
2020-08-23 15:15:33 +0200

    Added ID to ExampleComment output model

--- WikiLibs.Models/Output/Examples/ExampleComment.cs
+++ WikiLibs.Models/Output/Examples/ExampleComment.cs
@@ -6,6 +6,7 @@ namespace WikiLibs.Models.Output.Examples
 {
     public class ExampleComment : GetModel<ExampleComment, Data.Models.Examples.ExampleComment>
     {
+        public long Id { get; set; }
         public long ExampleId { get; set; }
         public string UserId { get; set; }
         public string Data { get; set; }
@@ -13,6 +14,7 @@ namespace WikiLibs.Models.Output.Examples
 
         public override void Map(in Data.Models.Examples.ExampleComment model)
         {
+            Id = model.Id;
             ExampleId = model.ExampleId;
             UserId = model.UserId;
             Data = model.Data;
