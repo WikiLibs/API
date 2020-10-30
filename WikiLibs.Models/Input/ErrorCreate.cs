acf27ec9f2bc596576f887d835c68d22fe15474c
Yuri Edward <nicolas1.fraysse@epitech.eu>
2020-10-30 16:12:30 +0100

    Added error POST Json IO model

---
+++ WikiLibs.Models/Input/ErrorCreate.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using WikiLibs.Data.Models;
+
+namespace WikiLibs.Models.Input
+{
+    public class ErrorCreate : PostModel<ErrorCreate, Error>
+    {
+        [Required]
+        public string ErrorMessage { get; set; }
+
+        [Required]
+        public string ErrorData { get; set; }
+
+        public override Error CreateModel()
+        {
+            return new Error()
+            {
+                ErrorData = ErrorData,
+                ErrorMessage = ErrorMessage
+            };
+        }
+    }
+}
