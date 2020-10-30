166fc69d5421b4210df4aefbcbe92b6f851147bc
Yuri Edward <nicolas1.fraysse@epitech.eu>
2020-10-30 09:52:30 +0100

    Added example vote database migration

---
+++ WikiLibs.Data/Migrations/20201030084556_AddExampleVotes.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace WikiLibs.Data.Migrations
+{
+    [ExcludeFromCodeCoverage]
+    public partial class AddExampleVotes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ExampleVotes",
+                columns: table => new
+                {
+                    UserId = table.Column<string>(nullable: false),
+                    ExampleId = table.Column<long>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ExampleVotes", x => new { x.UserId, x.ExampleId });
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ExampleVotes");
+        }
+    }
+}
